3586cefb6ef947fd5a210b4985b3fee634104989
mebeingprj-byte <mebeingprj@gmail.com>
2025-10-23 03:27:17 +0530

    Create Testimonials.jsx

---
+++ src/components/src/components/src/components/src/components/src/components/src/components/src/components/Testimonials.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Testimonials() {
+  return (
+    <div className="text-center" data-aos="fade-up">
+      <h2 className="font-montserrat text-4xl font-bold text-white mb-8 relative inline-block">
+        What Our Users Say
+        <span className="block absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-[#ffd700] rounded-full mt-2"></span>
+      </h2>
+      <p className="font-poppins text-lg text-gray-300 max-w-2xl mx-auto mb-10">
+        (Testimonials section placeholder. Real user feedback will be shown here.)
+      </s_p>
+    </div>
+  );
+}
+
+export default Testimonials;
