d75d91d9445efa65ca5e0c21b10a16e4f3d82af4
mebeingprj-byte <mebeingprj@gmail.com>
2025-10-23 03:23:48 +0530

    Create LeaderboardCard.jsx

---
+++ src/components/src/components/src/components/src/components/src/components/LeaderboardCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function LeaderboardCard() {
+  return (
+    <div className="bg-[#0a1f44] border border-[#c0c0c0]/30 rounded-lg shadow-xl p-6" data-aos="fade-up">
+      <h3 className="font-poppins text-2xl font-semibold text-white mb-3 text-center">
+        Leaderboard
+      </h3>
+      <p className="font-roboto text-gray-300 text-center">
+        (Leaderboard component placeholder. This will be an interactive feature.)
+      </p>
+    </div>
+  );
+}
+
+export default LeaderboardCard;
