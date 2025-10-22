da34fe8b40e6b276f9fbcc085e52a625052c8f6a
mebeingprj-byte <mebeingprj@gmail.com>
2025-10-23 03:25:22 +0530

    Create ChallengeCard.jsx

---
+++ src/components/src/components/src/components/src/components/src/components/src/components/ChallengeCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ChallengeCard() {
+  return (
+    <div className="bg-[#0a1f44] border border-[#c0c0c0]/30 rounded-lg shadow-xl p-6" data-aos="fade-up" data-aos-delay="150">
+      <h3 className="font-poppins text-2xl font-semibold text-white mb-3 text-center">
+        Daily Challenge
+      </h3>
+      <p className="font-roboto text-gray-300 text-center">
+        (Daily challenge component placeholder. This will be an interactive feature.)
+      </p>
+    </div>
+  );
+}
+
+export default ChallengeCard;
